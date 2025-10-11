a59d832f4dfd668e5fce2f4b4709e054fccf4c23
Irgi Kaliska <irgikaliska24@gmail.com>
2025-10-11 21:43:45 +0700

    Task 2

---
+++ jobsheet05/oddEven.java
@@ -0,0 +1,20 @@
+package jobsheet05;
+
+import java.util.Scanner;
+
+public class oddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+
+        sc.close();
+    }
+}
