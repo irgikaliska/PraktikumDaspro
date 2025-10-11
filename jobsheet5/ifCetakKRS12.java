3e2712fd57cd58456762b038344d84f8426c12d0
Irgi Kaliska <irgikaliska24@gmail.com>
2025-10-11 18:07:16 +0700

    Experiment 1

---
+++ jobsheet5/ifCetakKRS12.java
@@ -0,0 +1,20 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ifCetakKRS12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Print KRS SIAKAD ---");
+        System.out.print("Have the tuition paid in full?: (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("UKT payment has been verified");
+            System.out.println("KRS now can be printed and you can ask the academic to sign it.");
+
+            sc.close();
+        }
+    }
+}
