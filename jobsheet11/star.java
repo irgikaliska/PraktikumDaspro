d3ffff38d21f4c14e0e892ab30866b6cb190bb50
Irgi Kaliska <irgikaliska24@gmail.com>
2025-10-28 07:35:05 +0700

    jobsheet 11

---
+++ jobsheet11/star.java
@@ -0,0 +1,15 @@
+package jobsheet11;
+
+import java.util.Scanner;
+public class star {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter the value of N: ");
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.print("*");
+        } 
+    }
+}
