d2fa4fd68d13768bd7e62408a85d3e8b29985c3d
Irgi Kaliska <irgikaliska24@gmail.com>
2025-11-04 08:04:48 +0700

    jobsheet 9

---
+++ jobsheet9/arrayNumbers.java
@@ -0,0 +1,18 @@
+package jobsheet9;
+
+public class arrayNumbers {
+    public static void main(String[] args) {
+        
+        int[] num  = new int[4];
+
+        num[0] = 5;
+        num[1] = 12;
+        num[3] = 7;
+        num[3] = 20;
+
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+    }
+}
