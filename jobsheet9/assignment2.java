6c30f75638a905ef835709d78043771441ad6d22
Irgi Kaliska <irgikaliska24@gmail.com>
2025-11-06 18:44:53 +0700

    jobsheet 9

---
+++ jobsheet9/assignment2.java
@@ -0,0 +1,41 @@
+package jobsheet9;
+
+import java.util.Scanner;
+
+public class assignment2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);      
+
+        
+        System.out.print("Enter the number of orders: ");
+        int numOrders = sc.nextInt();
+        sc.nextLine(); 
+        
+        
+        String[] orderNames = new String[numOrders];
+        double[] orderPrices = new double[numOrders];
+        
+        // Input: Order details
+        for (int i = 0; i < numOrders; i++) {
+            System.out.print("Enter the name of order " + (i + 1) + ": ");
+            orderNames[i] = sc.nextLine();
+            System.out.print("Enter the price of order " + (i + 1) + ": Rp ");
+            orderPrices[i] = sc.nextDouble();
+            sc.nextLine();
+        }
+        
+        double totalCost = 0;
+        for (double price : orderPrices) {
+            totalCost += price;
+        }
+        
+        System.out.println("List of Orders:");
+        for (int i = 0; i < numOrders; i++) {
+            System.out.println((i + 1) + ". " + orderNames[i] + " - Rp" + orderPrices[i]);
+        }
+        System.out.println("Total Cost: Rp " + totalCost);
+        
+        sc.close();
+    }
+}
+
---
+++ jobsheet9/report.md
@@ -0,0 +1,18 @@
+## Question 1
+1. The program wont ran because the array's data type is integer and there is a decimal number.
+
+3. The output will be printed, because we use a loop to print all of the index at once.
+
+4. The program will ran to an error because the element of the index is only from 0 to 3.
+
+## Question 2 
+
+1. No there are no changes, because the array length is still 10.
+
+2. i < finalScore.length means that i is less than the length of the array elements.
+
+## Question 4
+
+1. the function of the break line is to break the loop once the loop is finished.
+
+2. 
